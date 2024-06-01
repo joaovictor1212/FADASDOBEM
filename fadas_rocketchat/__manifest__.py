b72ccb5d9341d2881d273042c4292d7afa43aecc
joaovictor1212 <joaovictor.1212@hotmail.com>
2024-06-01 18:59:20 -0300

    rocketchat

---
+++ fadas_rocketchat/__manifest__.py
@@ -0,0 +1,31 @@
+{
+    'name': 'Fadas do Bem RocketChat', 
+    'summary': 'Modulo para integração com o RocketChat',
+    'description': '''
+        Módulo de integração com o RocketChat
+    ''',
+    'version': '1.0.0', 
+    'category': 'Tools/UI',
+    'license': 'LGPL-3', 
+    'author': 'João Victor',
+    'website':'https://fadasdobem.com.br',
+    'depends': [
+        'base',
+        'web','website'
+    ],
+    'data': [
+        'views/website_templates.xml',
+        
+    ],
+    # 'qweb': [
+    #     'static/src/xml/radio_button_widget.xml',
+    # ],
+    'installable': True,
+    'application': True,
+    'auto_install':False,
+    # 'assets': {
+    #     'web.assets_backend': [
+    #         'comissoes_lugo_tech/static/src/js/assets.xml',
+    #     ],
+    # },
+}
