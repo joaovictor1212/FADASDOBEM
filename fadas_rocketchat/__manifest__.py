4e2828a471c4b76aedf8bf3a5dee9a5ccdbd7f8b
joaovictor1212 <joaovictor.1212@hotmail.com>
2024-06-01 20:20:15 -0300

    atualizaçao

--- fadas_rocketchat/__manifest__.py
+++ fadas_rocketchat/__manifest__.py
@@ -1,24 +1,28 @@
 {
     'name': 'Fadas do Bem RocketChat', 
     'summary': 'Modulo para integração com o RocketChat',
-    'description': '''
-        Módulo de integração com o RocketChat
-    ''',
+    'description': """
+        <p>Módulo de integração com o RocketChat.</p>
+        <p>Este módulo permite a integração direta com o RocketChat, permitindo que você adicione um widget de chat ao seu website Odoo.</p>
+        <ul>
+            <li>Facilita a comunicação com os visitantes do site</li>
+            <li>Integração simples e rápida</li>
+            <li>Totalmente customizável</li>
+        </ul>
+    """,
     'version': '1.0.0', 
     'category': 'Tools/UI',
     'license': 'LGPL-3', 
     'author': 'João Victor',
     'website':'https://fadasdobem.com.br',
     'depends': [
-        'website'
+        'website',
     ],
     'data': [
         'views/website_templates.xml',
         
     ],
-    # 'qweb': [
-    #     'static/src/xml/radio_button_widget.xml',
-    # ],
+    'images': ['static/description/icon.png'],
     'installable': True,
     'application': True,
     'auto_install':False,
